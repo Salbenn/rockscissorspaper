7d929e4aadc09d70e018f6c8522493c924668943
Student <jsitbon@qcc.cuny.edu>
2018-11-29 18:00:19 -0500

    app working w cosmetics

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import PlayerCard from './PlayerCard'
 
-
 class Game extends Component {
-  render() {
-    return (
+  constructor() {
+    super();
+    this.signs =['rock', 'scissors', 'paper']
+    this.state = {
+      playerOne: 'rock',
+      playerTwo: 'scissors',
+    }
+  }
+playGame=() => {
+  this.setState({
+    playerOne: this.signs[Math.floor(Math.random() * 3)],
+    playerTwo: this.signs[Math.floor(Math.random() * 3)],
+  })
+}
+
+decideWinner = () => {
+  const playerOne = this.state.playerOne;
+  const playerTwo = this.state.playerTwo;
+  if (playerOne === playerTwo){
+    return " it's a tie!"
+  }
+  else if ((palyerOne === "rock" && playerTwo === "scissors") ||
+  (playerOne === "paper" && playerTwo === "rock") ||
+  (playerOne === "scissors" && playerTwo ==="paper")) {
+  return " Player 1 wins!"
+}else {
+  return "Player 2 wins!"
+}
+}
+    render(){
+      return(
       <div className="style">
         <div>
-          <PlayerCard />
-          <PlayerCard />
+          <PlayerCard ={this.state.palyerOne}/>
+          <PlayerCard ={this.state.playerTwo}/>
         </div>
-        <div className = "winner">Put the winner here</div>
-        <button type="button">Play the Game</button>
+        <div className = "winner">{this.decideWinner()}</div>
+        <button type="button" onClick = {this.playGame}>Play the Game</button>
       </div>
 
     );
   }
 }
 
-export default App;
+export default Game;
--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+
+const scissors - "https://i.imgur.com/pgjyhIZ.png";
+const rock = "https://i.imgur.com/LghSkIw.png";
+const paper = "https://i.imgur.com/2gsdqvR.png";
 const PlayerCard = (props) => {
+  console.log(props)
   return (
-    <div className = "player-card"></div>
+    <div className = "player-card">
+      <img src={rock}/>
+    </div>
   )
 }
 
